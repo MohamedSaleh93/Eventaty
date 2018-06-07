dad28806fa38f562fc252d6b86e79737e01e1fa7
Mohamed Saleh <mohamedsaleh.fcis@gmail.com>
2018-06-07 12:18:02 +0200

    first commit that add project files into git

---
+++ app/src/main/java/com/task/eventaty/ui/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.task.eventaty.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author Mohamed Saleh on 6/7/2018.
+ */
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
